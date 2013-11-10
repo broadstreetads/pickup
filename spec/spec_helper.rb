9b1ba49bf77d5595c372a9860108f3d91be7501f
John Crepezzi <john.crepezzi@gmail.com>
2013-11-10 14:45:59 -0500

    Use a custom RSpec context

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,19 @@
 require_relative '../lib/pickup_line'
 
-def thoughts(today, &block)
-  describe "on #{today}" do
-    block.call(Proc.new { |question, reference|
-      it "should see '#{question}' as #{reference}" do
-        d = PickupLine::Parser.new(today)
-        ref = d.locate question
-        ref.should == reference
-      end
-    })
+# The methods available in the think context
+module ThinkMethods
+  def think(question, reference)
+    it "should see '#{question}' as #{reference}" do
+      date = Date.new(2013, 9, 28)
+      parser = PickupLine::Parser.new(date)
+      parser.locate(question).should == reference
+    end
   end
+end
+
+# A context method
+def thoughts(today, &block)
+  example_class = describe "on #{today}"
+  example_class.send :extend, ThinkMethods
+  example_class.class_eval &block
 end
